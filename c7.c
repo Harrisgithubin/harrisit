7f2614e8b5219550deee2fb4552ab4782d139ae6
A mohammed harris <115166073+Harrisgithubin@users.noreply.github.com>
2022-10-07 20:31:35 +0530

    Add files via upload

---
+++ c7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//int addNumbers(int a, int b);
+int addNumbers(int a, int b)
+{
+int result=20;
+
+return result;
+return 10;
+}
+void main()
+{
+int n1,n2,sum;
+
+
+sum = addNumbers(10,20);
+printf("%d",sum);
+}
